faab3ebe9f64a7f7c421dc904efb4a7fd47a6151
Brayan Cantos <brayancantosm07ci@gmail.com>
2025-03-21 18:43:15 -0500

    ✨ (core): reorganize core imports by removing unnecessary helpers module and updating exports to improve project structure and maintainability

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -2,6 +2,5 @@ export 'app/app.dart';
 export 'di/di.dart';
 export 'extensions/extensions.dart';
 export 'fp/fp.dart';
-export 'helpers/helpers.dart';
 export 'navigation/navigation.dart';
 export 'shared/shared.dart';
